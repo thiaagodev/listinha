077c7242092e0aec167e2f945c2edfcec35466be
Thiago Ferreira <thiaago.dev@gmail.com>
2023-02-07 20:50:39 -0300

    Aula 1

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -3,6 +3,5 @@ import 'dart:io';
 import 'package:backend/backend.dart' as backend;
 
 void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
   stdin.readLineSync();
 }
