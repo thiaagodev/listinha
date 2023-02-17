092e97a6aa7d9915ff498c569f4f4e88fa697f30
Thiago Ferreira <thiaago.dev@gmail.com>
2023-02-17 17:51:32 -0300

    relacionamento de tabelas no realm

--- app/lib/src/shared/services/reaml/reaml_config.dart
+++ app/lib/src/shared/services/reaml/reaml_config.dart
@@ -1,9 +1,13 @@
 import 'package:listinha/src/shared/services/reaml/models/configuration_model.dart';
 import 'package:realm/realm.dart';
 
+import 'models/task_model.dart';
+
 LocalConfiguration config = Configuration.local(
   [
     ConfigurationModel.schema,
+    Task.schema,
+    TaskBoard.schema,
   ],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
