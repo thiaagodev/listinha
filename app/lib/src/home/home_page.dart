fdf5b32b11969ad24dff2efefa9c6ea6dd6553f3
Thiago Ferreira <thiaago.dev@gmail.com>
2023-02-20 11:27:30 -0300

    Criação de Widget customizado

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:listinha/src/home/widgets/custom_drawer.dart';
+import 'package:listinha/src/home/widgets/task_card.dart';
+import 'package:listinha/src/shared/services/reaml/models/task_model.dart';
+import 'package:realm/realm.dart';
 
 import '../shared/widgets/user_image_button.dart';
 
@@ -28,35 +31,68 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Center(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: SegmentedButton<int>(
-                segments: const [
-                  ButtonSegment(
-                    value: 0,
-                    label: Text('Todos'),
-                  ),
-                  ButtonSegment(
-                    value: 1,
-                    label: Text('Pendentes'),
-                  ),
-                  ButtonSegment(
-                    value: 2,
-                    label: Text('Concluídas'),
-                  ),
-                  ButtonSegment(
-                    value: 3,
-                    label: Text('Desativadas'),
-                  ),
-                ],
-                selected: {selectedButton},
-                onSelectionChanged: (values) {
-                  setState(() {
-                    selectedButton = values.first;
-                  });
-                },
+            ListView.separated(
+              padding: const EdgeInsets.only(
+                top: 80,
+                left: 50,
+                right: 50,
+                bottom: 200,
+              ),
+              itemCount: 100,
+              itemBuilder: (_, index) {
+                final board = TaskBoard(
+                  Uuid.v4(),
+                  'Nova Lista de Tarefas 1',
+                  tasks: [
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '')
+                  ],
+                );
+                return TaskCard(
+                  board: board,
+                  height: 140,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 10,
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SegmentedButton<int>(
+                  segments: const [
+                    ButtonSegment(
+                      value: 0,
+                      label: Text('Todos'),
+                    ),
+                    ButtonSegment(
+                      value: 1,
+                      label: Text('Pendentes'),
+                    ),
+                    ButtonSegment(
+                      value: 2,
+                      label: Text('Concluídas'),
+                    ),
+                    ButtonSegment(
+                      value: 3,
+                      label: Text('Desativadas'),
+                    ),
+                  ],
+                  selected: {selectedButton},
+                  onSelectionChanged: (values) {
+                    setState(() {
+                      selectedButton = values.first;
+                    });
+                  },
+                ),
               ),
             )
           ],
