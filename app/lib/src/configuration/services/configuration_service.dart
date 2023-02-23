243a005520b6a2a820956a61865563a8d5926fa6
Thiago Ferreira <thiaago.dev@gmail.com>
2023-02-23 11:41:13 -0300

    reatividade com RxNotifier

--- app/lib/src/configuration/services/configuration_service.dart
+++ app/lib/src/configuration/services/configuration_service.dart
@@ -1,36 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:realm/realm.dart';
+import 'package:rx_notifier/rx_notifier.dart';
 
 import '../../shared/services/reaml/models/configuration_model.dart';
+import '../../shared/stores/app_store.dart';
 
 abstract class ConfigurationService {
-  ConfigurationModel getConfiguration();
-
-  void saveConfiguration(String themeModeName, DateTime? syncDate);
+  void init();
 
   void deleteAll();
 }
 
-class ConfigurationServiceImpl implements ConfigurationService {
+class ConfigurationServiceImpl implements ConfigurationService, Disposable {
   final Realm realm;
+  final AppStore appStore;
+  late final RxDisposer disposer;
 
-  ConfigurationServiceImpl(this.realm);
+  ConfigurationServiceImpl(this.realm, this.appStore);
+
+  @override
+  void init() {
+    final model = _getConfiguration();
+    appStore.themeMode = _getThemeModeByName(model.themeModeName);
+    appStore.syncDate = model.syncDate;
+
+    disposer = rxObserver(() {
+      final themeMode = appStore.themeMode;
+      final syncDate = appStore.syncDate;
+
+      _saveConfiguration(themeMode.name, syncDate);
+    });
+  }
 
   @override
   void deleteAll() {
     realm.deleteAll();
   }
 
-  @override
-  ConfigurationModel getConfiguration() {
+  ConfigurationModel _getConfiguration() {
     return realm.all<ConfigurationModel>().first;
   }
 
   @override
-  void saveConfiguration(String themeModeName, DateTime? syncDate) {
-    final model = getConfiguration();
+  void _saveConfiguration(String themeModeName, DateTime? syncDate) {
+    final model = _getConfiguration();
     realm.write(() {
       model.syncDate = syncDate;
       model.themeModeName = themeModeName;
     });
+  }
+
+  ThemeMode _getThemeModeByName(String name) {
+    return ThemeMode.values.firstWhere((mode) => mode.name == name);
+  }
+
+  @override
+  void dispose() {
+    disposer();
   }
 }
